0eb17e8368e66e35f9d8a957e83ab5ac433daccb
kushedow <Polemika2020>
2019-10-28 01:52:14 +0300

    Add route about

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def hello():
     return 'Hello World!'
+
+@app.route('/about')
+def about():
+    return 'Now it should work!'
 
 if __name__ == '__main__':
     app.run()
