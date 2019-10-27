30925a85945071ff2e2bab443b9c352da9a0ad66
kushedow <Polemika2020>
2019-10-28 01:44:48 +0300

    Added missed files to commit

---
+++ data.py
@@ -0,0 +1,81 @@
+#Declaring variables
+
+videos = {
+
+    "1":{
+        "id":1,
+        "title": "Как построить открытую террасу к дому своими руками",
+        "videoid": "hDX4hWWV5Ok",
+        "tags": ["дерево", "терраса", "сад"]
+    },
+    "2": {
+        "id": 2,
+        "title": "How To Learn Anything 10x Faster",
+        "description": "Interested in learning more about Charisma University? You’re going to spend thousands and thousands hours learning over the course of your life.  What’s crazy is that despite the fact that we have all those hours sitting in the classroom, learning musical instruments, martial arts, business, language, whatever - no one teaches us how to optimize that learning experience.",
+        "videoid": "YLwFbwPbq",
+        "tags": ["education", "parents", "course"]
+    },
+    "3": {
+        "id": 3,
+        "title": "How to draw to remember more",
+        "description": "How to draw to remember more! You may not believe it yet, but we are all capable of drawing sketches that make information memorable. In this talk you will discover talents you may have never known you had. Be prepared to amaze yourself!",
+        "videoid": "gj3ZnKlHqxI",
+        "tags": ["education", "projectbased", "curriculum"]
+    }
+
+}
+
+
+tags = [
+    "дерево","мастерская","инструмент","металл","хранение","ремонт"
+]
+
+
+playlists = {
+
+    "workshop": {
+
+        "id": "workshop",
+        "title": "Оборудование для мастерской своими руками",
+        "picture":"/static/images/1.jpg",
+
+        "videos": [
+
+            {"id":0,"title":"Строительство столярной мастерской или реконструкция сарая за 4 дня"},
+            {"id":1,"title":"Верстак в мастерскую. Обустройство столярной мастерской, часть 1"},
+            {"id": 2, "title": "Фрезерный стол своими руками | Обустройство столярной мастерской, часть 2"},
+            {"id": 3, "title": "Изготовление каретки для распиловочного стола | Приспособление для циркулярки своими руками"},
+            {"id": 4, "title": "Длиннее - лучше! Расширяем верстак в мастерской | Система хранения инструмента "},
+            {"id": 5, "title": "Система хранения инструмента французские планки | Обустройство столярной мастерской"},
+            {"id": 6, "title": "Обзор инструмента от девушки | Распиловочный стол Bosch PTS 10"},
+            {"id": 7, "title": "Токарный станок по дереву из фанеры своими руками | Часть 1 "},
+            {"id": 8, "title": "Токарный станок по дереву из фанеры своими руками | Часть 2"},
+            {"id": 9, "title": "Универсальный складной верстак для мастерской | Обзор верстака KREG"}
+        ]
+    },
+
+    "casting": {
+
+        "id": "casting",
+        "title": "Литье металла",
+        "picture":"/static/images/2.jpg",
+        "videos": [
+
+            {},{},{},{},{},{},{},{},{},{}, {}
+
+        ]
+    },
+
+    "woodwork": {
+
+        "id": "woodwork",
+        "title": "Работа по дереву",
+        "picture": "/static/images/3.jpg",
+        "videos": [
+
+            {}, {}, {}, {}, {}, {}, {}, {}, {}
+
+        ]
+    },
+
+}
