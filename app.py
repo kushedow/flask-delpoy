c3976bdf1f70fd7b782afbd303ee45671dfbd2d8
kushedow <Polemika2020>
2019-10-28 04:20:02 +0300

    Intentional mistake in about route

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return undefined
+
+
+
 
 if __name__ == '__main__':
     app.run()
